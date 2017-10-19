2bf90d8f63d9c2ecbf21ab80f95e608033cf891b
¨MaurodeLyon¨ <maurodelyon@gmail.com>
2017-10-19 17:00:24 +0200

    removal

--- robot_ws/build/joy_handler/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/ROS-project-MI/robot_ws/src/joy_handler
-# Build directory: /home/ros/ROS-project-MI/robot_ws/build/joy_handler
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- robot_ws/build/rosaria/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/ROS-project-MI/robot_ws/src/rosaria
-# Build directory: /home/ros/ROS-project-MI/robot_ws/build/rosaria
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
